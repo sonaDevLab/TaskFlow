331f6be481061b350a3c33313aca297a9af8b421
BlackFrisian13 <sonaivanyan13@gmail.com>
2026-02-23 14:16:48 +0100

    CommandLineRunner test succeed!

--- src/main/java/com/example/taskflow/TaskflowApp.java
+++ src/main/java/com/example/taskflow/TaskflowApp.java
@@ -1,13 +1,43 @@
 package com.example.taskflow;
 
+import com.example.taskflow.model.Task;
+import com.example.taskflow.model.User;
+import com.example.taskflow.service.TaskService;
+import com.example.taskflow.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TaskflowApp {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskflowApp.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(UserService userService, TaskService taskService){
+        return args -> {
+
+            User user = new User();
+            user.setName("Sona");
+            user.setEmail("sona@gmail.com");
+            user.setPassword("1234");
+
+            userService.createUser(user);
+
+            Task task = new Task();
+            task.setTitle("Terminar libro");
+            task.setDescription("Terminar de leer mi libro actual");
+            task.setUser(user);
+
+            taskService.createTask(task);
+
+            System.out.println("Tareas del usuario:");
+            taskService.getTasksByUserId(user.getId())
+                    .forEach(t -> System.out.println(t.getTitle()));
+        };
     }
 
 }
