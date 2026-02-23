ccdfe3c11b799dd13c003ed626724f5aaceb49f4
BlackFrisian13 <sonaivanyan13@gmail.com>
2026-02-23 13:53:14 +0100

    User service - create and get User methods created.

---
+++ src/main/java/com/example/taskflow/service/UserService.java
@@ -0,0 +1,32 @@
+package com.example.taskflow.service;
+
+import com.example.taskflow.model.User;
+import com.example.taskflow.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    //Constructor con inyección de dependencias
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    //Crear usuario
+    public User createUser(User user){
+        Optional<User> existing = userRepository.findByEmail(user.getEmail());
+        if(existing.isPresent()){
+            throw new RuntimeException("El email ya existe.");
+        }
+        return userRepository.save(user);
+    }
+
+    //Obtener usuario por ID
+    public User getUserById(Long id){
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario no encontrado."));
+    }
+}
