8829dd02373db359ad6f986ea99f38948b9061d0
BlackFrisian13 <sonaivanyan13@gmail.com>
2026-03-03 14:40:02 +0100

    RegisterForm created

---
+++ frontend/src/components/RegisterForm.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { register } from "../services/authService.js";
+
+function RegisterForm({ onRegister, onSwitch}) {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            await register(name, email, password);
+            onRegister(); // vuelve al login
+        } catch (error) {
+            setError("Error al registrar usuario");
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Registro</h2>
+
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+
+            <input
+                type='text'
+                placeholder='Nombre'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+            />
+
+            <input
+                type='email'
+                placeholder='Email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+            />
+
+            <input
+                type='password'
+                placeholder='Contraseña'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+            />
+
+            <button type='submit'>Registrarse</button>
+
+            <p>
+                ¿Ya tienes una cuenta?{" "}
+                <button type='button' onClick={onSwitch}>
+                    Inicia sesión
+                </button>
+            </p>
+        </form>
+    );
+}
+
+export default RegisterForm;
