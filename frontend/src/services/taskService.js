8b452d17240240452ce7df51218494999d4a0807
BlackFrisian13 <sonaivanyan13@gmail.com>
2026-03-04 15:47:42 +0100

    TaskService.js modified to include update and delete methods

--- frontend/src/services/taskService.js
+++ frontend/src/services/taskService.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8080/tasks";
 
-export const createTask = async (task) => {
+export const createTask = async (userId, task) => {
     try {
-        const response = await axios.post(API_URL, task);
+        const response = await axios.post(`${API_URL}/${userId}`, task);
         return response.data;
     } catch (error) {
         if(error.response) throw error.response.data;
@@ -13,6 +13,30 @@ export const createTask = async (task) => {
 };
 
 export const getTasksByUserId = async (userId) => {
-    const response = await axios.get(`${API_URL}/user/${userId}`);
-    return response.data;
+    try {
+        const response = await axios.get(`${API_URL}/${userId}`);
+        return response.data;
+    } catch (error) {
+        if(error.response) throw error.response.data;
+        throw error;
+    }
+};
+
+export const updateTask = async (userId, taskId, task) => {
+    try {
+        const response = await axios.put(`${API_URL}/${userId}/${taskId}`, task);
+        return response.data;
+    } catch (error) {
+        if(error.response) throw error.response.data;
+        throw error;
+    }
+};
+
+export const deleteTask = async (userId, taskId) => {
+    try {
+        await axios.delete(`${API_URL}/${userId}/${taskId}`);
+    }  catch (error) {
+        if(error.response) throw error.response.data;
+        throw error;
+    }
 };
