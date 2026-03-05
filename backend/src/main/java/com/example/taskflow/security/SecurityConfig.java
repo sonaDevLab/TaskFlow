6c0e3ed6cf47831128b441718b20e027f2d4fc6d
BlackFrisian13 <sonaivanyan13@gmail.com>
2026-03-05 21:32:02 +0100

    SecurityConfig update to execute JwtAuthFilter and authorize only authenticated users.

--- backend/src/main/java/com/example/taskflow/security/SecurityConfig.java
+++ backend/src/main/java/com/example/taskflow/security/SecurityConfig.java
@@ -5,23 +5,40 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+
+    //Define la cadena de filtros de seguridad
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+            .csrf(AbstractHttpConfigurer::disable) // CRSF no es necesario
+            .sessionManagement(session -> // info del usuario se guarda en el toke, no en el servidor.
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .authorizeHttpRequests(auth -> auth // /auth/* público. todolo demás requiere login.
+                    .requestMatchers("/auth/**").permitAll()
+                    .anyRequest().authenticated()
+            )
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class); // ejecuta filtro de autenticación antes del estándar.
 
-        return http.build();
+        return http.build(); //construye configuración final.
     }
 
+    // Encriptación de contraseñas con BCrypt
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
